733b31f0e0c34249d5d7c1674cfdcc919fb0221d
gayathmanawodi <nwgnabesiri@students.nsbm.ac.lk>
2026-03-29 12:04:08 +0530

    patient profile part 1

---
+++ hms_app/lib/view/patient/patient_profile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hospitrack/widgets/app_back_button.dart';
+import 'package:hospitrack/app/theme.dart';
+import 'package:hospitrack/controllers/auth_controller.dart';
+import 'package:hospitrack/widgets/shared_widgets.dart';
+import 'package:provider/provider.dart';
+
+class PatientProfile extends StatelessWidget {
+  const PatientProfile({super.key});
