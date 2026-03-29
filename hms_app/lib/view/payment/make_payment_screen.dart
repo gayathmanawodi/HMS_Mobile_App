44763fcec171ab464459bd00456e72592e2d7fc0
N. Sandunsara <sandunsara2002@gmail.com>
2026-03-29 14:08:52 +0530

    add MakePaymentScreen base structure

---
+++ hms_app/lib/view/payment/make_payment_screen.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:hospitrack/widgets/app_back_button.dart';
+import 'package:hospitrack/app/theme.dart';
+
+class MakePaymentScreen extends StatefulWidget {
+  const MakePaymentScreen({super.key});
+
+  @override
+  State<MakePaymentScreen> createState() => _MakePaymentScreenState();
+}
+
+class _MakePaymentScreenState extends State<MakePaymentScreen> {
+  String _method = 'Credit Card';
+  bool _paid = false;
